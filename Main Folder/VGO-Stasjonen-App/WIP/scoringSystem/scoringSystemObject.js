6a377b16f6649b0af0f02d9a5ab6f034a83684f3
Jamie Kielland <jamie@freshfx.no>
2020-05-28 14:41:34 +0200

    update 6

--- Main Folder/VGO-Stasjonen-App/WIP/scoringSystem/scoringSystemObject.js
+++ Main Folder/VGO-Stasjonen-App/WIP/scoringSystem/scoringSystemObject.js
@@ -1,9 +1,38 @@
 let scoreObject = 
 [
     {   
-        userId: "", 
-        objScore: "",
-        testId: "",     
+        userId: "3204", 
+        objScore: "34",
+        testId: "søvn",     
     }
 ];
 
+//---------------------Table script-------
+
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+  
+  function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
+  
+  let table = document.querySelector("table");
+  let data = Object.keys(scoreObject[0]);
+  generateTable(table, scoreObject);
+  generateTableHead(table, data);
