3403bc1a9b7414432c143d4f46d4576f12ae7ace
Jamie Kielland <jamie@freshfx.no>
2020-06-30 22:10:24 +0200

    SSome ish

---
+++ Main Folder/VGO-Stasjonen-App/Admin_side_VGO/JsScripts/Tests.js
@@ -0,0 +1,16 @@
+function adminTestPreview() {
+workAreaDiv.innerHTML = `
+<div class="admin_test_preview_container">
+    <div id="omOssDiv" class="admin_tests_style"> 
+ 
+        <select name="Kategori" id="kategori">
+            <option value="Søvn">Søvn</option>
+            <option value="Kosthold">Kosthold</option>
+            <option value="Fysisk Aktivitet">Fysisk Aktivitet</option>
+            <option value="Sosial Aktivitet">Sosial Aktivitet</option>
+            <option value="Økonomi">Økon</option>
+            <option value="Karrierveiledning og Utdanningsvalg">Karrierveiledning og Utdanningsvalg</option>
+        </select>
+    </div>
+</div>`   
+}
--- Main Folder/VGO-Stasjonen-App/js/login.js
+++ Main Folder/VGO-Stasjonen-App/js/login.js
@@ -1,24 +1,3 @@
-// function loginView(){
-//     headerDiv.style.display='none'
-//     innholdDiv.innerHTML=
-//     `   <h2>Velkommen til VGO-Stasjonen</h2>
-//         <div class="loginbox" id="formContainer">Username:
-//         <input type="email" placeholder="Username" id="email">
-//         Password: <input type="password" placeholder="Password" id="password">
-
-//         <button id="signIn" onclick="signIn();model.page='main';show();"> Sign In </button>
-//         </div>`
-//     }
-
-//     function signIn(){
-//         headerDiv.style.display='block';
-//         showMainPageHeader();
-//         showMainPageContentView();
-        
-
-//     }
-    //<button onclick="signOut()" id="signOut"> Sign Out </button>
-    //<button onclick="signUp()" id="signUp"> Sign Up </button>
 
     function loginView(){
         headerDiv.style.display='none'
@@ -38,7 +17,8 @@
                 </div>
             <input type="submit" class="btn" value="Submit" onclick="signIn();model.page='main';show();">
         </form>
-    
+        <br>
+            <button class="adminLoginBtn" onclick="location.href = 'adminIndex.html'">Admin Login</button>
         <ul id="users"></ul>
 
     </section>
